0cf75938526ac75cdad3d0918c2e0ea86af0eae5
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-06 20:24:42 +0600

    Added modules and initialzed post

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,5 @@
-import express, { type Application } from "Express"
+import express, { type Application } from "express"
 export const app: Application = express();
+app.get("/", (req, res) => {
+    res.send("Hello World ")
+})
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import { app } from "./app";
 import { prisma } from "./lib/prisma";
-
+const PORT = process.env.PORT || 5000;
 async function main() {
+
     try {
         await prisma.$connect();
         console.log("Successfully Connect to Database");
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        })
     } catch {
         console.error("Doesn't Connect to Database")
+        await prisma.$disconnect();
+        process.exit(1)
     }
 }
 main();
