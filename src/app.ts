e899eb29648f0969b709a8152317f4d5bf378898
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-06 20:27:07 +0600

    Added modules and initialzed post connect app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express, { type Application } from "express"
+import express, { Router, type Application } from "express"
+import { postRouter } from "./modules/Post/post.router";
 export const app: Application = express();
+app.use(express.json())
+// post 
+app.use("/posts", postRouter)
 app.get("/", (req, res) => {
     res.send("Hello World ")
 })
