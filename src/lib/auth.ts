35a4fa007ac3bb458407e07600de1f81a743acfa
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-07 21:47:45 +0600

    added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,29 @@ import { prisma } from "./prisma";
 
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
- 
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  trustedOrigins: [process.env.APP_URL!],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+        required: false
+      },
+      phone: {
+        type: "string",
+        required: false
+      },
+      status: {
+        type: "string",
+        defaultValue: "Active",
+        required: false
+      }
+    }
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
 });
