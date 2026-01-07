9bcbe9d2a5b898cae5f6eb90e9b4598568559be3
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-07 21:22:02 +0600

    Added  better auth configuretion

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Router, type Application } from "express"
 import { postRouter } from "./modules/Post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 export const app: Application = express();
 app.use(express.json())
 // post 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use("/posts", postRouter)
 app.get("/", (req, res) => {
     res.send("Hello World ")
