b9eb98c7e721a4ab0e6c83f059140ea9e0ead903
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-08 00:31:18 +0600

    added type global;

--- src/modules/Post/post.router.ts
+++ src/modules/Post/post.router.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 //     "ADMIN", "USER"
 // }
 declare global {
-    namespace express {
+    namespace Express {
         interface Request {
             user?: {
                 id: string,
@@ -23,21 +23,28 @@ declare global {
 const auth = (...roles: any) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         console.log(roles);
-        const sesstion = await betterAuth.api.getSession({
+        const session = await betterAuth.api.getSession({
             headers: req.headers as any
         })
-        console.log(sesstion);
-        if (!sesstion) {
+        console.log(session);
+        if (!session) {
             return res.status(401).json({
                 success: false,
                 message: "Your Are Not Autorized"
             })
         }
-        if (!sesstion.user.emailVerified) {
+        if (!session.user.emailVerified) {
             return res.status(401).json({
                 success: false,
                 message: "Email Verifcation Required. Please verify your email"
             })
+        }
+        req.user = {
+            id: session.user.id,
+            email: session.user.email,
+            name: session.user.name,
+            role: session.user.role as string,
+            emailVerified : session.user.emailVerified
         }
 
         next();
