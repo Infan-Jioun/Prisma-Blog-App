18a648dd8356bfa7c07507dfaac5dcf98c84ea33
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-07 22:30:27 +0600

    added nodemailer

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,16 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
+  },
+});
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -33,7 +42,9 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendVerificationEmail: async ({ user, url, token }, request) => {
+      // ekane teke email sender use kora hoi
       console.log("Send Verification email");
     },
   }
+
 });
