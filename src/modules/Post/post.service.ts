76f4439702de03786f575683aa74cac174a73aba
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-09 15:42:44 +0600

    distructure and addConditions push added

--- src/modules/Post/post.service.ts
+++ src/modules/Post/post.service.ts
@@ -1,29 +1,40 @@
 import type { Post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const getAllPost = async (payload: { search: string | undefined, tags: string[] | [] }) => {
-    const allpost = await prisma.post.findMany({
-        where: {
+const getAllPost = async ({ search, tags }: { search: string | undefined, tags: string[] | [] }) => {
+    const addConditions = [];
+    if (search) {
+        addConditions.push({
             OR: [
                 {
                     title: {
-                        contains: payload.search as string,
+                        contains: search as string,
                         mode: "insensitive"
                     }
                 },
                 {
                     content: {
-                        contains: payload.search as string,
+                        contains: search as string,
                         mode: "insensitive"
                     }
                 },
                 {
                     tags: {
-                        has: payload.search as string
+                        has: search as string
                     }
                 }
 
-            ]
+            ],
+        })
+    }
+
+    const allpost = await prisma.post.findMany({
+
+        where: {
+
+            tags: {
+                hasEvery: tags as string[]
+            }
         }
     });
     return allpost;
