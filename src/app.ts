cc38e76fb778db641880d9e25c4850e7be817b00
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-12 11:56:35 +0600

    create comment router , controller , service and connnect to app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { postRouter } from "./modules/Post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import cors from 'cors';
+import { commentRouter } from "./modules/Comments/comment.router";
 export const app: Application = express();
 
 app.use(cors({
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // post 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use("/posts", postRouter)
+app.use("/comments", commentRouter)
 app.get("/", (req, res) => {
     res.send("Hello World ")
 })
