ed5529a75339399be65ac4fcfc7f6b3f9e34995e
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-14 14:21:27 +0600

    added post router

--- src/modules/Post/post.router.ts
+++ src/modules/Post/post.router.ts
@@ -7,8 +7,9 @@ const router = express.Router();
 // middlewere
 
 router.get("/", postController.getAllPost)
-router.get("/my-post", auth(UserRole.USER , UserRole.ADMIN), postController.getMyPost)
+router.get("/my-post", auth(UserRole.USER, UserRole.ADMIN), postController.getMyPost)
 router.post("/", auth(UserRole.USER), postController.createPost)
 router.get("/:id", postController.getPostById)
+router.get("/:postId", auth(UserRole.ADMIN, UserRole.USER), postController.updatePost)
 
 export const postRouter: Router = router; 
