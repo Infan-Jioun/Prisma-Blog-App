26878f49e97f70b90c94589d711262593371b769
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-01-15 14:21:24 +0600

    connect  dlobalErrorHandler with app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import cors from 'cors';
 import { commentRouter } from "./modules/Comments/comment.router";
+import { errorHandler } from "./middlewere/globalErrorHandler";
 export const app: Application = express();
 
 app.use(cors({
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use("/posts", postRouter)
 app.use("/comments", commentRouter)
+app.use(errorHandler)
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
